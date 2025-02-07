e30b63b866045629bdc6c588e3af395ebe980953
cmd-anurag <anuragkumar.7157@gmail.com>
2025-02-07 19:44:10 +0530

    Revert "Merge pull request #6 from cmd-anurag/UI-polish"
    This reverts commit 8b3243c04713ab4599ca0e4ee7ceaa8510f93d99, reversing
    changes made to d95ab209a46b101981498b3d30746d73ace83e51.
    
    revert #6

--- home.js
+++ home.js
@@ -1,11 +1,3 @@
 const mobileMenuBtn = document.getElementById("mobile-menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
-
-mobileMenuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-});
-document.addEventListener("click", (event) => {
-    if (!mobileMenu.contains(event.target) && !mobileMenuBtn.contains(event.target)) {
-        mobileMenu.classList.add("hidden");
-    }
-});
+mobileMenuBtn.addEventListener("click", () => { mobileMenu.classList.toggle("hidden");});
