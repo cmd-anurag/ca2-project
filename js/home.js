e8d71800dedb8a49df646e5bba7e689522077e4a
Suvigya <rai.suvigya3@gmail.com>
2025-02-14 11:32:16 +0530

    added verifyotp page

---
+++ js/home.js
@@ -0,0 +1,16 @@
+const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+const closeBtn = document.getElementById("close-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+mobileMenuBtn.addEventListener("click",()=>{
+    mobileMenu.classList.toggle("hidden");
+});
+document.addEventListener("click", (event)=>{
+    if(!mobileMenu.contains(event.target) && !mobileMenuBtn.contains(event.target)){
+        mobileMenu.classList.add("hidden");
+    }
+});
+
+closeBtn.addEventListener("click",()=>{
+    mobileMenu.classList.add("hidden");
+});
