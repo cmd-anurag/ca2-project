b08b738760e285acc18a555dc1f70e1e13899873
Suvigya <rai.suvigya3@gmail.com>
2025-04-06 22:42:48 +0530

    modified Co-authored-by: Anurag Kumar <cmd-anurag@users.noreply.github.com>

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,110 +4,121 @@ const nameField = document.getElementById("name-input");
 const emailField = document.getElementById("email-input");
 const passwordField = document.getElementById("password-input");
 
+const invalidName = document.getElementById("invalidname");
+const invalidEmail = document.getElementById("invalidemail");
+const invalidPassword = document.getElementById("invalidpass");
+const registeredEmail = document.getElementById("registered-email");
+
+
 let loader = document.getElementById("loader-element");
 
 function isValidEmail(email) {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
 }
 
 // SIGNUP BUTTON CLICK LISTENER
-signupbtn.addEventListener("click", async () => {    
+signupbtn.addEventListener("click", async () => {
+  if (!nameField.value) {
+    invalidName.classList.remove("hidden");
+    return;
+  }
+  if (!emailField.value || !isValidEmail(emailField.value)) {
+    invalidEmail.classList.remove("hidden");
+    return;
+  }
 
-    if(!nameField.value) {
-        nameField.classList.add("ring-2", "ring-red-500", "text-red-500");
-        nameField.placeholder = "A name is required.";
-        return
+  if (!passwordField.value || passwordField.value.length < 8) {
+    invalidPassword.classList.remove("hidden");
+    return;
+  }
+
+  loader.classList.remove("hidden");
+
+  // check if the email already exists
+
+  let emailres = false;
+
+  try {
+    const formdata = new FormData();
+    formdata.append("email", emailField.value);
+    emailres = await fetch(
+      "http://localhost/ca2-project/backend/verify_email.php",
+      {
+        method: "POST",
+        credentials: "include",
+        body: formdata,
+      }
+    );
+
+    const result = await emailres.json();
+
+    if (!result.success) {
+      registeredEmail.classList.remove('hidden');
+      return;
     }
-    if(!emailField.value) {
-        emailField.classList.add("ring-2", "ring-red-500", "text-red-500");
-        emailField.placeholder = "Email cannot be empty.";
-        return;
+  } catch (error) {
+    console.log(error);
+  }
+  finally{
+    loader.classList.add("hidden");
+  }
+
+  let response;
+  try {
+    const formdata = new FormData();
+    formdata.append("name", nameField.value);
+    formdata.append("email", emailField.value);
+    formdata.append("password", passwordField.value);
+
+    response = await fetch(
+      "http://localhost/ca2-project/backend/send_otp.php",
+      {
+        method: "POST",
+        credentials: "include",
+        body: formdata,
+      }
+    );
+    const result = await response.json();
+
+    if (result.success) {
+      console.log("sent otp");
+      window.location.href = result.redirect;
+    } else {
+      console.error("some error occured");
     }
-
-    if(!isValidEmail(emailField.value)) {
-        emailField.classList.add("ring-2", "ring-red-500", "text-red-500");
-        emailField.placeholder = "Please enter a valid email.";
-        emailField.value = "";
-        return;
-    }
-
-    if(!passwordField.value) {
-        passwordField.classList.add("ring-2", "ring-red-500", "text-red-500");
-        passwordField.placeholder = "Password is required.";
-        return;
-    }
-
-    loader.classList.remove("hidden");
-
-
-    // check if the email already exists
-
-    let emailres = false;
-    
-    try {
-        const formdata = new FormData();
-        formdata.append("email", emailField.value);
-        emailres = await fetch("http://localhost/ca2-project/backend/verify_email.php", {
-            method : "POST",
-            credentials : "include",
-            body : formdata
-        });
-
-        const result = await emailres.json();
-
-        if(!result.success) {
-            loader.classList.add("hidden");
-            emailField.classList.add("ring-2", "ring-yellow-500");
-            emailField.placeholder = "This email is already registered as a user.";
-            emailField.value = "";
-            return;
-        }
-    }
-    catch(error) {
-        loader.classList.add("hidden");
-        console.log(error);
-    }
-
-    let response;
-    try {
-        
-        const formdata = new FormData();
-        formdata.append("name", nameField.value);
-        formdata.append("email", emailField.value);
-        formdata.append("password", passwordField.value);
-    
-        response = await fetch("http://localhost/ca2-project/backend/send_otp.php", {
-            method : "POST",
-            credentials: "include",
-            body : formdata
-        })
-        const result = await response.json();
-    
-        if(result.success) {
-            console.log("sent otp");
-            window.location.href = result.redirect;
-        }
-        else {
-            console.error("some error occured");
-        }
-    }
-    catch(error) {
-        loader.classList.add("hidden");
-        console.log(error);
-    }
-})
-
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loader.classList.add("hidden");
+  }
+});
 
 nameField.addEventListener("focus", () => {
-    nameField.placeholder = "";
-    nameField.classList.remove("ring-2", "ring-red-500", "text-red-500");
-})
+  invalidName.classList.add("hidden");
+});
 emailField.addEventListener("focus", () => {
-    emailField.placeholder = "";
-    emailField.classList.remove("ring-2", "ring-red-500", "text-red-500");
-})
+  invalidEmail.classList.add('hidden');
+  registeredEmail.classList.add('hidden');
+});
 passwordField.addEventListener("focus", () => {
-    passwordField.placeholder = "";
-    passwordField.classList.remove("ring-2", "ring-red-500", "text-red-500");
-})
+  invalidPassword.classList.add('hidden');
+});
+
+const toggleBtns = document.querySelectorAll(".password-toggle");
+toggleBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const input = btn.previousElementSibling;
+    const icon = btn.querySelector("i");
+
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    } else {
+      input.type = "password";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    }
+  });
+});
